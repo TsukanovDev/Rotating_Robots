6959e9ea0a49d1b0cb3de795d9ccdd7fe1ee0bf0
Viktor <tsukanov87@mail.ru>
2023-05-05 14:33:15 +0300

    `

--- script.js
+++ script.js
@@ -36,18 +36,16 @@ window.addEventListener('load', function () {
 			//body
 			//рисуем изображение 
 			context.drawImage(this.bodyImage, this.x - this.bodyImage.width * 0.5 + 65, this.y - this.bodyImage.height * 0.5 - 53);
-			context.beginPath();
-			context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-			context.stroke();
+
 			//eye1
-			context.drawImage(this.eye1Image, this.x + Math.cos(this.angle) * this.radius * 0.35 - this.eye1Image.width * 0.5, this.y + Math.sin(this.angle) * this.radius * 0.35 - this.eye1Image.width * 0.5);
-			context.beginPath();
-			context.arc(this.x + Math.cos(this.angle) * this.radius * 0.35, this.y + Math.sin(this.angle) * this.radius * 0.35, this.radius * 0.6, 0, Math.PI * 2);
-			context.stroke();
+			context.drawImage(this.eye1Image, this.x + Math.cos(this.angle) * this.radius * 0.4 - this.eye1Image.width * 0.5, this.y + Math.sin(this.angle) * this.radius * 0.4 - this.eye1Image.width * 0.5);
+
 			//eye2
-			context.beginPath();
-			context.arc(this.x + Math.cos(this.angle) * this.radius * 0.6, this.y + Math.sin(this.angle) * this.radius * 0.6, this.radius * 0.3, 0, Math.PI * 2);
-			context.stroke();
+			context.drawImage(this.eye2Image, this.x + Math.cos(this.angle) * this.radius * 0.65 - this.eye2Image.width * 0.5, this.y + Math.sin(this.angle) * this.radius * 0.65 - this.eye2Image.width * 0.5);
+			//reflection
+			//рисуем изображение 
+			context.drawImage(this.reflectionImage, this.x - this.reflectionImage.width * 0.5, this.y - this.reflectionImage.height * 0.5);
+
 		}
 		update() {
 			//углы вокруг объектов
